fdcac509239d4319cbb1578da128c66299c58ed2
LAPTOP-QIDO0NP0\Howard Mui <mhw19972013@gmail.com>
2021-12-14 10:08:18 +0800

    Testing commit

--- pages/index.js
+++ pages/index.js
@@ -1,8 +1,87 @@
-import Head from 'next/head'
-import Image from 'next/image'
-import styles from '../styles/Home.module.css'
+import Head from "next/head";
+import Image from "next/image";
+import styles from "../styles/Home.module.css";
 
-export default function Home() {
+import React, { useState, useEffect } from "react";
+// import Image from "next/image";
+// import moment from "moment";
+import Link from "next/link";
+import { request, gql } from "graphql-request";
+
+const graphqlAPI =
+  "https://api-ap-northeast-1.graphcms.com/v2/ckwsga7el479z01z10tjr979t/master";
+// "https://api-eu-central-1.graphcms.com/v2/cku56f92114s901yz0ce9ah3f/master";
+
+const testing = async () => {
+  // const query = gql`
+  //   query MyQuery2 {
+  //     postsConnection {
+  //       edges {
+  //         node {
+  //           author {
+  //             name
+  //           }
+  //         }
+  //       }
+  //     }
+  //   }
+  // `;
+
+  const query = gql`
+    query findSlug {
+      posts {
+        slug
+      }
+    }
+  `;
+
+  // const query = gql`
+  //   query MyQuery {
+  //     postsConnection {
+  //       edges {
+  //         node {
+  //           author {
+  //             bio
+  //             name
+  //             id
+  //             photo {
+  //               url
+  //             }
+  //           }
+  //           createdAt
+  //           slug
+  //           title
+  //           excerpt
+  //           featuredImage {
+  //             url
+  //           }
+  //           categories {
+  //             name
+  //             slug
+  //           }
+  //         }
+  //       }
+  //     }
+  //   }
+  // `;
+
+  const result = await request(graphqlAPI, query);
+  const data = result.posts;
+  console.log(data);
+  // const results = result.postsConnection.edges;
+
+  return {
+    props: {
+      data,
+    },
+  };
+};
+
+// testing();
+
+const Home = ({ data }) => {
+  // testing();
+  console.log(data);
   return (
     <div className={styles.container}>
       <Head>
@@ -15,55 +94,9 @@ export default function Home() {
         <h1 className={styles.title}>
           Welcome to <a href="https://nextjs.org">Next.js!</a>
         </h1>
-
-        <p className={styles.description}>
-          Get started by editing{' '}
-          <code className={styles.code}>pages/index.js</code>
-        </p>
-
-        <div className={styles.grid}>
-          <a href="https://nextjs.org/docs" className={styles.card}>
-            <h2>Documentation &rarr;</h2>
-            <p>Find in-depth information about Next.js features and API.</p>
-          </a>
-
-          <a href="https://nextjs.org/learn" className={styles.card}>
-            <h2>Learn &rarr;</h2>
-            <p>Learn about Next.js in an interactive course with quizzes!</p>
-          </a>
-
-          <a
-            href="https://github.com/vercel/next.js/tree/master/examples"
-            className={styles.card}
-          >
-            <h2>Examples &rarr;</h2>
-            <p>Discover and deploy boilerplate example Next.js projects.</p>
-          </a>
-
-          <a
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className={styles.card}
-          >
-            <h2>Deploy &rarr;</h2>
-            <p>
-              Instantly deploy your Next.js site to a public URL with Vercel.
-            </p>
-          </a>
-        </div>
       </main>
-
-      <footer className={styles.footer}>
-        <a
-          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Powered by{' '}
-          <span className={styles.logo}>
-            <Image src="/vercel.svg" alt="Vercel Logo" width={72} height={16} />
-          </span>
-        </a>
-      </footer>
     </div>
-  )
-}
+  );
+};
+
+export default Home;
